f8447187fe11c1958a9cd54454099714b6763b41
yesid <euspiritus@gmail.com>
2019-03-18 13:27:30 -0500

    primer commit de mi perfil profesional

---
+++ js/filtro.js
@@ -0,0 +1,58 @@
+/* Aqui le damos a nuestros botones activar cuando seleccionemos click sobre ellos,
+Tambien escondiendo nuestras imagenes y solo mostrando las del boton seleccionado 
+*/
+$(function(){
+    $('.filter').click(function(){
+        $(this).addClass('active').siblings().removeClass('active');
+        let valor = $(this).attr('data-nombre');
+        if(valor == 'todos'){
+            $('.cont-work').show('1000');
+        }else{
+            $('.cont-work').not('.'+ valor).hide('1000');
+            $('.cont-work').filter('.'+ valor).show('1000');
+        }
+    });
+    
+    let equipo = $('#equipo').offset().top,
+        servicio = $('#servicio').offset().top,
+        trabajo = $('#trabajo').offset().top,
+        contacto = $('#contacto').offset().top;
+    
+    window.addEventListener('resize', function(){
+        let equipo = $('#equipo').offset().top,
+        equipo = $('#servicio').offset().top,
+        equipo = $('#trabajo').offset().top,
+        equipo = $('#contacto').offset().top;   
+    });
+
+    $('#enlace-inicio').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        },600);
+    });
+    $('#enlace-equipo').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: equipo -100
+        }, 600);
+    });
+     $('#enlace-servicio').on('click', function (e) {
+         e.preventDefault();
+         $('html, body').animate({
+             scrollTop: servicio - 100
+         }, 600);
+     });
+    $('#enlace-trabajo').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: trabajo - 100
+        }, 600);
+    });
+    $('#enlace-contacto').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: contacto - 100
+        }, 600);
+    });
+});
